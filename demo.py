9edc75919cedd4d7a7fce6a718106b17b3eaf177
swastik2216 <158576078+swastik2216@users.noreply.github.com>
2026-05-07 16:33:03 +0530

    Uhm ddi a few codes in day 0 files

---
+++ demo.py
@@ -0,0 +1,6 @@
+#Q1
+print("Hello World!"); print("Have a good day."); print("Bye bye!")
+"""
+You can use the print() function as many times as you want.
+Each call prints text on a new line by default
+"""
